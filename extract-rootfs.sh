7d0db1dd12320f4fcefa4c7dadc65331ff03891f
Alex Davies <traverse.da@gmail.com>
2025-08-20 16:58:18 -0300

    feat: add extract-rootfs.sh script

---
+++ extract-rootfs.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p erofs-utils bash curl
+
+
+rootfs-path=https://github.com/home-assistant/operating-system/releases/download/16.1/haos_generic-x86-64-16.1.raucb
